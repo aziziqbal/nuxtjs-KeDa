1a9f6ce6d7f52b1beb445f0c466a1bb1b6d581c3
Muh Aziz Iqbal <muhaziziqbal06@gmail.com>
2021-10-07 20:02:53 +0700

    setup unit test

--- test/radio.spec.js
+++ test/radio.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Logo from '@/components/RadioButton.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
